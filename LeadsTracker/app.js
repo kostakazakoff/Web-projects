b8a5e48603e36ca66880b8c25fccd25efdf790a4
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-04 15:51:56 +0200

    optimising

--- LeadsTracker/app.js
+++ LeadsTracker/app.js
@@ -5,6 +5,7 @@ const deleteBtn = document.getElementById('delete');
 const ulEl = document.getElementById('leads-container');
 let myLeads = [];
 leadsFromLocalStorage = JSON.parse(localStorage.getItem('Leads'));
+console.log(leadsFromLocalStorage)
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
@@ -12,23 +13,28 @@ if (leadsFromLocalStorage) {
 }
 
 saveInputBtn.addEventListener('click', function() {
-    myLeads.push(inputEl.value);
-    inputEl.value = '';
+    if (inputEl.value && !myLeads.includes(inputEl.value)) {
+        myLeads.push(inputEl.value);
+        inputEl.value = '';
     localStorage.setItem('Leads', JSON.stringify(myLeads));
     render(myLeads);
+    }
+})
+
+deleteBtn.addEventListener('click', function() {
+    localStorage.clear()
+    render(myLeads)
 })
 
 function render(items) {
-    let listItems = ''
+    let listItems = '';
     for (let i = 0; i < items.length; i++) {
         listItems += `
         <li>
-            <a href='${items[i]}' target='_blank>
-                ${items[i]}
-            </a>
-        </li>
-        `
+            <a href="${items[i]}" target="_blank">${items[i]}</a>
+        </li>   
+        `;
     }
-    console.log(listItems)
-    ulEl.innerHTML = listItems
+    console.log(listItems);
+    ulEl.innerHTML = listItems;
 }
