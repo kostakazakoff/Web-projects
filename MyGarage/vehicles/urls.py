0e8d352f37f4548adfb5e568a5756a5043944ced
Kosta Kazakov <kostakazakoff@gmail.com>
2023-05-20 17:56:31 +0300

    refactoring

--- MyGarage/vehicles/urls.py
+++ MyGarage/vehicles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+
 from vehicles.views import index, service
 
 urlpatterns = [
     path('', index),
-    path('service', service, name='service'),
+    path('service/', service, name='service'),
     path('service/<int:vehicle_id>/', service, name='service'),
 ]
