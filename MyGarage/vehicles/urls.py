c747b0fc56f1b68f9b24dff4a1e97ba110a86e0b
Kosta Kazakov <kostakazakoff@gmail.com>
2023-05-23 06:20:38 +0300

    learn Django

--- MyGarage/vehicles/urls.py
+++ MyGarage/vehicles/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from vehicles.views import index, service
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
     path('service/', service, name='service'),
     path('service/<int:vehicle_id>/', service, name='service'),
 ]
