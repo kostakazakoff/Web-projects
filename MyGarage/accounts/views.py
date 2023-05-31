292e07c2f306354deb7d4625fb4d40ee07276997
Kosta Kazakov <kostakazakoff@gmail.com>
2023-06-01 01:23:32 +0300

    working on

--- MyGarage/accounts/views.py
+++ MyGarage/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def login_page(request):
+    return render(request, 'accounts/login-page.html')
+
+def create_account(request):
+    return render(request, 'accounts/create-account.html')
+
+def edit_account(request, id):
+    return render(request, 'accounts/edit-account.html')
+
+def delete_account(request, id):
+    return render(request, 'accounts/delete-account.html')
