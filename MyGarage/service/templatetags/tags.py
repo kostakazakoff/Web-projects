36ae1735b80d2b40d8c879eee5adf78fbf35a9fa
Kosta Kazakov <kostakazakoff@gmail.com>
2023-06-01 15:44:08 +0300

    optimising templates

--- MyGarage/service/templatetags/tags.py
+++ MyGarage/service/templatetags/tags.py
@@ -2,7 +2,7 @@ from django.template import Library
 from datetime import datetime
 register = Library()
 
-@register.inclusion_tag(filename='service/service.html', name='app_nav')
+@register.inclusion_tag('tags/nav.html', name='app_nav')
 def generate_nav(*args):
     context = {
         'url_names': args
