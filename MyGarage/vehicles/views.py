20a3b891bfca16837acf70fb3fdfeb82056e3b1a
Kosta Kazakov <kostakazakoff@gmail.com>
2023-06-03 22:36:22 +0300

    working on

--- MyGarage/vehicles/views.py
+++ MyGarage/vehicles/views.py
@@ -6,11 +6,11 @@ from django.shortcuts import get_object_or_404
 def garage(request, *args, **kwargs):
     search_str = request.GET.get('header__search_field')
     service_field = []
-    
+
     if search_str:
-        all_vehicles = Vehicles.objects.filter(brand__icontains=search_str) \
-        or Vehicles.objects.filter(vin__contains=search_str) \
-        or Vehicles.objects.filter(plate__icontains=search_str)
+        all_vehicles = Vehicles.objects.filter(brand__icontains=search_str) or\
+        Vehicles.objects.filter(vin__contains=search_str) or\
+        Vehicles.objects.filter(plate__icontains=search_str)
     else:
         all_vehicles = Vehicles.objects.all()
 
@@ -19,28 +19,32 @@ def garage(request, *args, **kwargs):
         if vehicle.vehicle_service.count() > 0:
             for service in vehicle.vehicle_service.all():
                 service_field.append(
-                    {'service_id': service.id,\
-                     'vehicle_id': service.vehicle.id,\
-                     'description': service.description,\
+                    {'service_id': service.id,
+                     'vehicle_id': service.vehicle.id,
+                     'description': service.description,
                      'date': service.date.strftime('%Y-%m-%d')}
-                    )
-    print(service_field)
+                )
+    # print(service_field)
     # ------------------------------------------------
-        
+
     context = {
         'vehicles': all_vehicles,
         'title': 'Garage',
-        'vehicles_service': service_field
-        }
+        'vehicles_service': service_field,
+        'criteria': 'Brand, VIN or Plate'
+    }
     return render(request, 'garage/garage.html', context)
+
 
 def add_vehicle(request):
     return render(request, 'garage/add-vehicle.html')
+
 
 def edit_vehicle(request, id):
     vehicle = get_object_or_404(Vehicles, pk=id)
     context = {'vehicle': vehicle}
     return render(request, 'garage/edit-vehicle.html', context)
+
 
 def delete_vehicle(request, id):
     vehicle = get_object_or_404(Vehicles, pk=id)
