79560dd50208a40207cbfbd8b762506ba2a6bdb9
Kosta Kazakov <kostakazakoff@gmail.com>
2023-06-08 00:25:17 +0300

    working on

--- MyGarage/my_garage/settings.py
+++ MyGarage/my_garage/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
 )
 
-# TODO: Media root
+# Media root
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 MEDIA_URL = 'mediafiles/'
 
