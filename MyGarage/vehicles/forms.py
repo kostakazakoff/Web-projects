298d9477d0b2719ebc461e25d0a0500018cd73c8
Kosta Kazakov <kostakazakoff@gmail.com>
2023-06-26 15:00:35 +0300

    Add fields placeholder for "add vehicle" field

--- MyGarage/vehicles/forms.py
+++ MyGarage/vehicles/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Vehicles
 
@@ -7,3 +8,12 @@ class CreateVehicleForm(ModelForm):
     class Meta:
         model = Vehicles
         exclude = ['slug']
+        widgets = {
+            'brand': forms.TextInput(attrs={'placeholder': 'Max 30 characters'}),
+            'model': forms.TextInput(attrs={'placeholder': 'Max 30 characters'}),
+            'vin': forms.TextInput(attrs={'placeholder': 'Enter the VIN code here'}),
+            'plate': forms.TextInput(attrs={'placeholder': 'Max 10 characters'}),
+            'odometer': forms.TextInput(attrs={'placeholder': 'Enter the odometer value here'}),
+            'year': forms.TextInput(attrs={'placeholder': 'Year of manufacture in format YYYY'}),
+            'date_of_purchase': forms.TextInput(attrs={'type': 'date'}),
+        }
