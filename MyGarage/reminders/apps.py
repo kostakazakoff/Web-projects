6585f80346a09df113c318a5da37b1f3dd952bfc
Kosta Kazakov <kostakazakoff@gmail.com>
2023-10-14 08:36:13 +0300

    Schedule time changed to 8:00

--- MyGarage/reminders/apps.py
+++ MyGarage/reminders/apps.py
@@ -11,6 +11,6 @@ class RemindersConfig(AppConfig):
         import reminders.signals # must import signals manualy (signals are not a part of Django convension)
         from .schedule import check_reminders
         scheduler = BackgroundScheduler()
-        scheduler.add_job(check_reminders, 'cron', hour=9, minute=20)
+        scheduler.add_job(check_reminders, 'cron', hour=8, minute=0)
         scheduler.start()
         return result
