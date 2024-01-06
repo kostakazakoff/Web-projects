cd9e71b99a67c6b592023cd51d130d7de1678cec
Kosta Kazakov <kostakazakoff@gmail.com>
2024-01-06 13:33:40 +0200

    comment removed

--- MyGarage/reminders/apps.py
+++ MyGarage/reminders/apps.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
-# TODO: Transform scheduling into Celery & Redis
 class RemindersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reminders'
